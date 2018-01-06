04566fe7b6bf781787039a0ec5b2734eb48782e7
stef <radusi.stefan@gmail.com>
2018-01-06 03:11:11 +0200

    img carusel

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-/* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/fromCloudinary', function(req, res, next) {
+  const params = req.query;  
+  console.log(params);
   request({
-    uri: 'https://252193543756474:lEWldoRle9zhG-B02O6m7Hgt--Y@api.cloudinary.com/v1_1/hofas67vr/resources/image'
+    uri: `https://252193543756474:lEWldoRle9zhG-B02O6m7Hgt--Y@api.cloudinary.com/v1_1/hofas67vr/resources/image?type=upload&prefix=${params.folder.replace('/', '\/')}&max_results=500`
   }, (error, response, body) => {
     if (error) {
       res.json(error);
@@ -14,6 +17,7 @@ router.get('/', function(req, res, next) {
       res.json(JSON.parse(body));
     }
   });
+
 
 });
 
